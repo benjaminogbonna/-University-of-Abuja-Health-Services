927fc027eee306bfc5b27112d287575a0e53663c
benjamin <onyedikachi1997@gmail.com>
2022-11-15 11:28:37 +0100

    admin panel edit

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Doctor
 
-admin.site.register(Doctor)
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('username', 'phone', 'department')
+    list_filter = ('department', 'gender',)
+    search_fields = ('username',)
--- patient/admin.py
+++ patient/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from . models import Patient, Appointment
 
-admin.site.register(Patient)
-admin.site.register(Appointment)
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('username', 'card_number', 'gender')
+    list_filter = ('level', 'gender',)
+    search_fields = ('card_number',)
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('doctor', 'patient', 'date', 'time')
+    list_filter = ('approved', 'approved',)
+    search_fields = ('doctor', 'patient')
