c13bc0e65185ecc40ffbc493845020c9cefffc0a
benjamin <onyedikachi1997@gmail.com>
2022-11-18 11:45:52 +0100

    worked on users changing password when logged in

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'main'
@@ -10,4 +11,22 @@ urlpatterns = [
     path('contact', views.contact, name='contact'),
     path('account/login/', views.user_login, name='login'),
     path('account/logout/', views.log_out, name='logout'),
+
+
+    # change password urls from account
+    path('account/password-change/',
+         auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('main:password_change_done')),
+         name='password_change'),
+    path('account/password-change/done/', auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
+
+    # reset password urls in case you forget
+    path('account/password-reset/', auth_views.PasswordResetView.as_view(
+        success_url=reverse_lazy('main:password_reset_done')), name='password_reset'),
+    path('account/password-reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('account/reset-password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('main:password_reset_complete')), name='password_reset_confirm'),
+    path('account/reset-password/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
--- patient/urls.py
+++ patient/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from django.urls import reverse_lazy
 from . import views
 
 app_name = 'patient'
@@ -9,24 +7,4 @@ urlpatterns = [
     path('patient/profile', views.patient_profile, name='profile'),
     path('patient/book-appointment', views.book_appointment, name='book_appointment'),
     path('patient/appointments', views.appointments, name='appointments'),
-
-
-
-    # change password urls from account
-    path('patient/password-change/',
-         auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('patient:password_change_done')),
-         name='password_change'),
-    path('patient/password-change/done/', auth_views.PasswordChangeDoneView.as_view(),
-         name='password_change_done'),
-
-    # reset password urls in case you forget
-    path('patient/password-reset/', auth_views.PasswordResetView.as_view(
-        success_url=reverse_lazy('patient:password_reset_done')), name='password_reset'),
-    path('patient/password-reset/done/', auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
-    path('patient/reset-password/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        success_url=reverse_lazy('patient:password_reset_complete')), name='password_reset_confirm'),
-    path('patient/reset-password/done/', auth_views.PasswordResetCompleteView.as_view(),
-         name='password_reset_complete'),
-
 ]
